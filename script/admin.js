2e23507184d278571ef87124aef4c11dcb135ea7
israel <israelwendellc@gmail.com>
2025-10-31 16:20:34 -0300

    feat:implement logic of storage of the value of event

--- script/admin.js
+++ script/admin.js
@@ -1,11 +1,23 @@
 const formEvent = document.getElementById("form");
 const storageEvent = JSON.parse(localStorage.getItem("event")) || [];
 
+let eventCardValue = document.getElementById("event-card-value");
+
+let value = storageEvent.length > 0 ? storageEvent[storageEvent.length - 1].id :0;  
+
+window.addEventListener("load", () => {
+    eventCardValue.textContent = value;
+})
+
+
 formEvent.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const formEventValue = {};
+    let formEventValue = {};
     
+    value++;
+    formEventValue = {...formEventValue, id:value};
+
     const formValues  = formEvent.querySelectorAll("input, select");
 
     formValues.forEach(formValue => {
@@ -15,4 +27,7 @@ formEvent.addEventListener("submit", (e) => {
     storageEvent.push(formEventValue);
 
     localStorage.setItem("event",JSON.stringify(storageEvent));
+    window.location.reload();
 })
+
+
