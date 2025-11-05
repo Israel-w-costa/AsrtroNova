f51310be815ceb788a972f72dcd2dd656525953c
Menegotti Douglas <douglasamaralmenegotti@gmail.com>
2025-11-05 17:56:41 -0300

    teste js

---
+++ agendamentojs.js
@@ -0,0 +1,25 @@
+let dadosAgendamento = JSON.parse(localStorage.getItem('dadosAgendamento')) || [];
+
+document.getElementById('formularioAgendamento').addEventListener('submit', e => {
+    e.preventDefault();
+
+    let nome = document.getElementById('nome').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let telefone = document.getElementById('telefone').value.trim();
+    let date = document.getElementById('date').value;
+    let hour = document.querySelector('input[name="hour"]:checked').value;
+    let number = Number(document.getElementById('number').value);
+
+    const agendamento = {
+        nome,
+        email,
+        telefone,
+        date,
+        hour,
+        number
+    };
+
+    dadosAgendamento.push(agendamento);
+
+    localStorage.setItem('dadosAgendamento', JSON.stringify(dadosAgendamento));
+});
